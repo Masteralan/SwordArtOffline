296b95229f15aeb6cc0bb865eb4e21b6d48cb723
Alan <masteralan2001@gmail.com>
2019-11-22 17:30:57 -0500

    Touched on JFrame

--- main.java
+++ main.java
@@ -12,7 +12,8 @@ class Main {
         //frame.setTitle("Sword Art Offline");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        frame.setLocationRelativeTo(null);
+        frame.setLocation(100, 100);
+        frame.setSize(800, 600);
         frame.pack();
         frame.setVisible(true);
 
