f738ff8e7f46568f12a0ff2423dcf9951db372be
Alan <masteralan2001@gmail.com>
2020-02-24 15:39:12 -0500

    Formatting, imports, added WASD control scheme, removed extraneous function/comments, added ESCAPE to close

--- src/Window.java
+++ src/Window.java
@@ -1,71 +1,74 @@
-import java.awt.Canvas;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-import java.awt.image.BufferStrategy;
-import java.awt.event.*;
 import javax.swing.*;
-import java.awt.Color;
-import java.util.*;
 import java.awt.*;
-import java.io.File;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferStrategy;
 
 public class Window implements Runnable {
     JFrame frame;
+
     int positionX = 400;
     int positionY = 0;
     int floor = 500;
+
     double velocityY = 0;
     double velocityX = 0;
     double accelerationX = 0;
     double accelerationY = 10;
     double friction = 0.6;
+
     Canvas canvas;
     BufferStrategy bufferStrategy;
+
     boolean running = true;
-    boolean up, down, left, right;
+    boolean up, down, left, right, attack;
+
+
     public Window() {
-      frame = new JFrame("frame");
-      JPanel panel = (JPanel) frame.getContentPane();
-      panel.setPreferredSize(new Dimension(1400, 800));
-      panel.setLayout(null);
-      canvas = new Canvas();
-      canvas.setBounds(0, 0, 1400, 800);
-      canvas.setIgnoreRepaint(true);
-      panel.add(canvas);
-      canvas.addKeyListener(new KeyAdapter() {
+        frame = new JFrame("Sword Art Offline");
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.setPreferredSize(new Dimension(1400, 800));
+        panel.setLayout(null);
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, 1400, 800);
+        canvas.setIgnoreRepaint(true);
+        panel.add(canvas);
+        canvas.addKeyListener(new KeyAdapter() {
+
           @Override
           public void keyPressed(KeyEvent evt) {
             int key =evt.getKeyCode();
-            if(key==KeyEvent.VK_DOWN) {
+            if(key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S)
               down = true;
-            }
-            if(key==KeyEvent.VK_UP) {
+            if(key == KeyEvent.VK_UP || key == KeyEvent.VK_W)
               up = true;
-            }
-            if(key==KeyEvent.VK_LEFT) {
+            if(key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A)
               left = true;
-            }
-            if(key==KeyEvent.VK_RIGHT) {
+            if(key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D)
               right = true;
+            if (key == KeyEvent.VK_SPACE)
+                attack = true;
+
+            if (key == KeyEvent.VK_ESCAPE) {  // Close window upon hitting escape
+                running = false;
+                frame.dispose();
             }
           }
+
           @Override
           public void keyReleased(KeyEvent evt) {
             int key = evt.getKeyCode();
-            if(key==KeyEvent.VK_DOWN) {
+            if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S)
               down = false;
-            }
-            if(key==KeyEvent.VK_UP) {
+            if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W)
               up = false;
-            }
-            if(key==KeyEvent.VK_LEFT) {
+            if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A)
               left = false;
-             }
-             if(key==KeyEvent.VK_RIGHT) {
+             if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D)
               right = false;
-            }
           }
       });
+
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.pack();
       frame.setResizable(false);
@@ -73,77 +76,54 @@ public class Window implements Runnable {
       canvas.createBufferStrategy(2);
       bufferStrategy = canvas.getBufferStrategy();
       canvas.requestFocus();
-  }
-  public void run() {
-    while (running = true) {
-      Paint();
-      try {
-        Thread.sleep(25);
-      } catch (InterruptedException e) {
-      }
     }
-  }
-  public static void main(String[] args) {
-      Thread.currentThread().setPriority((int)(Thread.MAX_PRIORITY));
-      Window game = new Window();
-      new Thread(game).start();
-  }
-  public void Paint() {
-      Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-      g.clearRect(0, 0, 1400, 800);
-      Paint(g);
-      bufferStrategy.show();
-  }
-  protected void Paint(Graphics2D g) {
+    public void run() {
+        while (running = true) {
+            Paint();
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
 
-    if(up) {
-      //positionY-=8;
-      if(positionY>=floor)
-        accelerationY-=20;
+            }
+        }
     }
-    if(down) {
-      //positionY+=8;
-      accelerationY++;
+    public static void main(String[] args) {
+        Thread.currentThread().setPriority((int)(Thread.MAX_PRIORITY));
+        Window game = new Window();
+        new Thread(game).start();
     }
-    if(left) {
-      //positionX-=8;
-      accelerationX--;
+    public void Paint() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, 1400, 800);
+        Paint(g);
+        bufferStrategy.show();
     }
-    if(right) {
-      //positionX+=8;
-      accelerationX++;
-    }
-    accelerationY/=1.1;//subject to change
-    accelerationX/=1.1;//subject to change
-    velocityX+=accelerationX - friction*velocityX;
-    velocityY+=accelerationY - friction*velocityY;
-    positionX = (int)velocityX+positionX;
-    if((int)velocityY+positionY>=floor)
-      positionY = floor;
-    else {
-      positionY = (int)velocityY+positionY;
-      accelerationY+=1;
+    protected void Paint(Graphics2D g) {
+        if(up && positionY>=floor)
+            accelerationY-=20;
+        if(down)
+          accelerationY++;
+        if(left)
+          accelerationX--;
+        if(right)
+          accelerationX++;
 
-    }
-    //Image img = Toolkit.getDefaultToolkit().getImage("person.png");
-    //g.drawImage(img, positionX,positionY,null);
-    g.setColor(Color.BLACK);
-    g.fillRect(positionX,positionY,50,50);
+        accelerationY/=1.1;//subject to change
+        accelerationX/=1.1;//subject to change
+        velocityX+=accelerationX - friction*velocityX;
+        velocityY+=accelerationY - friction*velocityY;
+        positionX = (int)velocityX+positionX;
+        if((int)velocityY+positionY>=floor)
+            positionY = floor;
+        else {
+            positionY = (int)velocityY+positionY;
+            accelerationY+=1;
+        }
 
-  }
-  public void moveIt(KeyEvent evt) {
-    int key =evt.getKeyCode();
-    if(key==KeyEvent.VK_DOWN) {
-      down = true;
-    }
-    if(key==KeyEvent.VK_UP) {
-      up = true;
-    }
-    if(key==KeyEvent.VK_LEFT) {
-      left = true;
-    }
-     if(key==KeyEvent.VK_RIGHT) {
-      right = true;
-    }
+        //Image img = Toolkit.getDefaultToolkit().getImage("person.png");
+        //g.drawImage(img, positionX,positionY,null);
+        g.setColor(Color.BLACK);
+        g.fillRect(positionX,positionY,50,50);
+
   }
 }
